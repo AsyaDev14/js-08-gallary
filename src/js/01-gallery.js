4f5ec183a57d844bbd787ce0c9daec7226f777ea
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-06 13:43:13 +0200

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from './gallery-items.js';
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGallery(items) {
+  return items.map(({ preview, original, description }) => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  }).join('');
+}
+galleryContainer.insertAdjacentHTML('beforeend', createGallery(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250
+});
+
