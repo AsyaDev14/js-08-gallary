d0f29a42e8a807bdb8180d6dda3a531e2a0c8ff0
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-07 21:12:52 +0200

    update task 3.formData

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,24 +2,32 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 let formData = {};
-const savedValues = localStorage.getItem(STORAGE_KEY);
-const savedDataObject = JSON.parse(savedValues);
-
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('.feedback-form  input'),
   textarea: document.querySelector('.feedback-form  textarea'),
 };
-// ?. - if null or undefind = don`t check 
-if (savedDataObject?.email) {
-  refs.form.elements.email.value = savedDataObject.email
-}
-if (savedDataObject?.message) {
-  refs.form.elements.message.value = savedDataObject.message
-}
 
 refs.form.addEventListener('input', throttle(onFormChange, 500));
 refs.form.addEventListener('submit', onFormSubmit);
+
+const getLocalStorage = () => {
+  const savedValues = localStorage.getItem(STORAGE_KEY);
+  const savedDataObject = JSON.parse(savedValues);
+  return savedDataObject;
+};
+
+const localData = getLocalStorage();
+
+// ?. - if null or undefind = don`t check a
+// check if smth in LocalStorage 
+
+if (localData?.email) {
+  refs.form.elements.email.value = localData.email
+}
+if (localData?.message) {
+  refs.form.elements.message.value = localData.message
+}
 
 function onFormChange() {
   const email = refs.form.elements.email.value;
@@ -38,7 +46,7 @@ function onFormChange() {
 
 function onFormSubmit(e) {
   e.preventDefault()
-  const data = savedDataObject;
+  const data = getLocalStorage();
   console.log("data", data);
   localStorage.removeItem(STORAGE_KEY);
   refs.form.elements.email.value = "";
