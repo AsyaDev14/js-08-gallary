01514ccf8de32d959663b0f3a30aea2ed0709561
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-07 20:10:35 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -10,8 +10,37 @@ const refs = {
   input: document.querySelector('.feedback-form  input'),
   textarea: document.querySelector('.feedback-form  textarea'),
 };
+// ?. - if null or undefind = don`t check 
+if (savedDataObject?.email) {
+  refs.form.elements.email.value = savedDataObject.email
+}
+if (savedDataObject?.message) {
+  refs.form.elements.message.value = savedDataObject.message
+}
 
-refs.form.addEventListener('input', throttle(storageFormData, 500));
+refs.form.addEventListener('input', throttle(onFormChange, 500));
 refs.form.addEventListener('submit', onFormSubmit);
 
+function onFormChange() {
+  const email = refs.form.elements.email.value;
+  const message = refs.form.elements.message.value;
+  if (email) {
+    formData.email = email;
+  }
 
+  if (message) {
+    formData.message = message
+  }
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+function onFormSubmit(e) {
+  e.preventDefault()
+  const data = savedDataObject;
+  console.log("data", data);
+  localStorage.removeItem(STORAGE_KEY);
+  refs.form.elements.email.value = "";
+  refs.form.elements.message.value = "";
+}
